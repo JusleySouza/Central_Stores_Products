9260719cadd1f4568afcbd015887aef8bc59a80d
Jusley Souza <jusleysouza31@gmail.com>
2023-02-06 18:08:14 -0300

    Classe de formatação de datas

---
+++ src/main/java/com/central/stores/products/constants/Conf.java
@@ -0,0 +1,10 @@
+package com.central.stores.products.constants;
+
+import lombok.Generated;
+
+@Generated
+public final class Conf {
+	
+	public static final String dateFormat = "yyyy-MM-dd HH:mm";
+	
+}
