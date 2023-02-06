820906c77f710cafefe874fc00701ea331324450
Jusley Souza <jusleysouza31@gmail.com>
2023-02-06 18:57:31 -0300

    Criação e implementação da rota de update

--- src/main/java/com/central/stores/products/controller/ProductController.java
+++ src/main/java/com/central/stores/products/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.central.stores.products.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +25,12 @@ public class ProductController {
 	public ResponseEntity<Object> create(@RequestBody RequestProductDTO requestProductDTO){
 		return services.create(requestProductDTO);
 	}
+	
+	@PutMapping("/{productId}")
+	public ResponseEntity<Object> update(@RequestBody RequestProductDTO requestProductDTO,
+			@PathVariable("productId") UUID productId){
+		return services.update(requestProductDTO, productId);
+	}
+
 
 }
