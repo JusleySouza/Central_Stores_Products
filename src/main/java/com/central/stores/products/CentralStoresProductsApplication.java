d80886d3263ff556be46fdda16e25e0f9bef86de
Jusley Souza <jusleysouza31@gmail.com>
2023-02-06 18:07:33 -0300

    Classe principal do projeto

---
+++ src/main/java/com/central/stores/products/CentralStoresProductsApplication.java
@@ -0,0 +1,13 @@
+package com.central.stores.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralStoresProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralStoresProductsApplication.class, args);
+	}
+
+}
