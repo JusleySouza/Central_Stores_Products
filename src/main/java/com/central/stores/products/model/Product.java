093ab173ccfe644b9464ad761596716f2d867a40
Jusley Souza <jusleysouza31@gmail.com>
2023-02-06 18:21:33 -0300

    Criação e implementação do modelo de produto

---
+++ src/main/java/com/central/stores/products/model/Product.java
@@ -0,0 +1,54 @@
+package com.central.stores.products.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.central.stores.products.constants.Conf;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(updatable = false, nullable = false )
+	private int id;
+	@Column(nullable = false )
+	private String name;
+	@Column(nullable = false )
+	private String batch;
+	@Column(nullable = false )
+	private int amount;
+	@Column(nullable = false )
+	private double price;
+	@Column(nullable = false )
+	private String size;
+	@Column(nullable = false )
+	@DateTimeFormat(pattern = Conf.dateFormat)
+	private LocalDate created;
+	@DateTimeFormat(pattern = Conf.dateFormat)
+	private LocalDate changed;
+
+}
